f974c4d5abf465f00c002e656d05a0dfafd426ef
xiwilson <linzi.wilson@hotmail.com>
2025-03-22 15:24:02 +0000

    Uploaded weather project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,74 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
-function App() {
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="weather-app">
+        <header>
+          <form className="search-form" id="search-form">
+            <input
+              type="search"
+              placeholder="🔍 Enter a city..."
+              required
+              id="search-form-input"
+              className="search-form-input"
+            />
+            <input
+              type="submit"
+              value="Search"
+              className="search-form-button"
+            />
+          </form>
+        </header>
+        <main>
+          <div className="weather-app-data">
+            <div>
+              <h1 className="weather-app-city" id="city">
+                London
+              </h1>
+              <p className="weather-app-details">
+                <span id="time">Monday 13:56,</span>
+                <span id="description">clear sky</span>
+                <br />
+                Humidity: <strong id="humidity">49%</strong>, Wind:
+                <strong id="wind-speed">3.09km/h</strong>
+              </p>
+            </div>
+            <div className="weather-app-temperature-container">
+              <div id="icon">
+                <img
+                  src="http://shecodes-assets.s3.amazonaws.com/api/weather/icons/clear-sky-day.png"
+                  className="weather-app-icon"
+                />
+              </div>
+              <div className="weather-app-temperature-value" id="temperature">
+                19
+              </div>
+              <div className="weather-app-unit">°C</div>
+            </div>
+          </div>
+          <div className="weather-forecast" id="forecast"></div>
+        </main>
+        <footer>
+          Coded by
+          <a href="https://github.com/xiwilson" target="_blank">
+            Linzi Wilson,
+          </a>
+          is open-sourced on
+          <a
+            href="https://github.com/xiwilson?tab=repositories"
+            target="_blank"
+          >
+            GitHub
+          </a>
+          and is
+          <a href="https://xiweather-app.netlify.app/" target="_blank">
+            hosted on Netlify
+          </a>
+        </footer>
+      </div>
+      <script src="src/app.js"></script>
     </div>
   );
 }
-
-export default App;
